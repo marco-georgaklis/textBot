b94c6ff5d7de3cd6a5032c1332fdb380d54db30d
Marco Georgaklis <marco.georgaklis@vanderbilt.edu>
2020-07-18 21:41:56 -0400

    removed extra files

--- config.py
+++
@@ -1,7 +0,0 @@
-account_sid = 'ACe37e3fd2fb3a5c8c7c5121a470073261'
-auth_token = 'a60c79b632db57a6b0481296673eca72'
-# reddit API
-redditUser = 'mx2017'
-redditPassword = 'Mgmg1234'
-redditSecret = "M3DnZJmnT9vr-4qWNP2fRZHlcfw"
-redditApp = "3yZuhcc3I1gliw"
